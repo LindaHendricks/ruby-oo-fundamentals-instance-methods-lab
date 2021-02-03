812fe094f7548aaf4d28e009a5d3df7a15b17e39
Linda Hendricks <linda.timoh@gmail>
2021-02-03 09:35:47 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,28 @@
+
+
+class Dog
+    def initialize
+    end 
+end 
+
+class Dog 
+    def bark
+        puts "Woof!"
+    end 
+
+    def sit 
+        puts "The Dog is sitting"
+    end 
+   
+end 
+
+linda = Dog.new
+
+puts linda.bark
+puts linda.sit
+
+
+
+
+
+
